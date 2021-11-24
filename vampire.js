b2a7661c6054576103a17ba92f0a3759b80e4804
github4maninder <maninder.email@gmail.com>
2021-11-23 19:01:56 -0500

    implement closestCommonAncestor

--- vampire.js
+++ vampire.js
@@ -4,28 +4,51 @@ class Vampire {
     this.yearConverted = yearConverted;
     this.offspring = [];
     this.creator = null;
+    this.descendents = 0;
   }
 
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
 
+    let vampireCount = 0;
+    let currentVampire = this;
+
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampireCount++;
+    }
+
+    return vampireCount;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
+  }
 
+  listOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let vampiresList = [[currentVampire.name, currentVampire]];
+
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      vampiresList.push([currentVampire.name, currentVampire]);
+    }
+
+    return vampiresList;
   }
 
   /** Stretch **/
@@ -37,8 +60,17 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    let thisVampireAncestors = this.listOfVampiresFromOriginal();
+    let anotherVampireAncestors = vampire.listOfVampiresFromOriginal();
+
+    for (let ancestor of thisVampireAncestors) {
+      for(let ancestor2 of anotherVampireAncestors){
+        if (ancestor[0] === ancestor2[0]){
+          return ancestor[1];
+        }
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
